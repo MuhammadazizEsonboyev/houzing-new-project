85914a5d8323b75706462599ae58a8616d219c28
MuhammadazizEsonboyev <esonboyev03@bk.ru>
2023-05-31 15:04:32 +0500

    fix

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Link, Logo, Section, Wrapper } from './style'
+import { Container, Link, Logo, Main, Section, Wrapper } from './style'
 import { Outlet, useNavigate } from 'react-router-dom'
 import navbar from '../../utils/navbar'
 import logoImg from '../../assets/images/logoimg.svg'
@@ -9,27 +9,29 @@ export const Navbar = () => {
 
   return (
     <Container>
-      <Wrapper>
-        <Section onClick={() => navigate('/home')} logo>
-          <Logo src={logoImg} />
-        </Section>
-        <Section>
-          {navbar?.map(({ title, path, id }, index) => {
-            return (
-              <Link
-                className={({ isActive }) => isActive && 'active'}
-                key={index}
-                to={path}
-              >
-                {title}
-              </Link>
-            )
-          })}
-        </Section>
-        <Section>
-          <button>Sign In</button>
-        </Section>
-      </Wrapper>
+      <Main>
+        <Wrapper>
+          <Section onClick={() => navigate('/home')} logo>
+            <Logo src={logoImg} />
+          </Section>
+          <Section>
+            {navbar?.map(({ title, path, id }, index) => {
+              return (
+                <Link
+                  className={({ isActive }) => isActive && 'active'}
+                  key={index}
+                  to={path}
+                >
+                  {title}
+                </Link>
+              )
+            })}
+          </Section>
+          <Section>
+            <button>Sign In</button>
+          </Section>
+        </Wrapper>
+      </Main>
       <Outlet />
     </Container>
   )
